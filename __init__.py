ee0f99c459f653e2a3bf840f071a216956c48b49
Erik de Jonge <erik@a8.nl>
2012-12-23 02:41:43 +0100

    init not on classlevel anymore

--- __init__.py
+++ __init__.py
@@ -72,108 +72,80 @@ def make_p_callable(the_callable, params):
                   "pickled_closure": pickle.dumps(the_callable.func_closure), "params": params}
     return p_callable
 
+
 class CryptoTask(SaveObject):
-    """ async execution, where the function 'run' is securely saved in couchdb. """
-
-    # the pickled executable
-
-    m_callable_p64s = None
-
-    # result after execution
-
-    m_result = ""
-
-    # execution done
-
-    m_done = False
-
-    # was the execution successful, false if an exception in the callable occurred
-
-    m_success = False
-
-    # class created
-
-    m_created_time = None
-
-    # time execution started
-
-    m_start_execution = None
-
-    # time execution stopped
-
-    m_stop_execution = None
-
-    # the signature of the pickled executable
-
-    m_signature_p64s = None
-
-    # max time the execution may run
-
-    m_max_lifetime = 60 * 5
-
-    # progress counter
-
-    m_progress = 0
-
-    # total for progress calculation
-
-    m_total = 100
-
-    # time in seconds the execution will take, for progress calculation
-
-    m_expected_duration = 0
-
-    # execution is running
-
-    m_running = False
-
-    # the object type
-
-    m_command_object = None
-
-    # public keys of the command queue
-
-    public_keys = []
-
-    # id of the user to which the task belongs
-
-    m_crypto_user_object_id = None
-
-    # data to operate on
-
-    m_process_data_p64s = None
-
-    # delete the task when completed
-
-    m_delete_me_when_done = True
-
     def __init__(self, dbase, object_id=None, crypto_user_object_id=None):
+        """ async execution, where the function 'run' is securely saved in couchdb. """
+
+        # the pickled executable
+        self.m_callable_p64s = None
+
+        # result after execution
+        self.m_result = ""
+
+        # execution done
+        self.m_done = False
+
+        # was the execution successful, false if an exception in the callable occurred
+        self.m_success = False
+
+        # class created
+        self.m_created_time = None
+
+        # time execution started
+        self.m_start_execution = None
+
+        # time execution stopped
+        self.m_stop_execution = None
+
+        # the signature of the pickled executable
+        self.m_signature_p64s = None
+
+        # max time the execution may run
+        self.m_max_lifetime = 60 * 5
+
+        # progress counter
+        self.m_progress = 0
+
+        # total for progress calculation
+        self.m_total = 100
+
+        # time in seconds the execution will take, for progress calculation
+        self.m_expected_duration = 0
+
+        # execution is running
+        self.m_running = False
+
+        # the object type
+        self.m_command_object = None
+
+        # public keys of the command queue
+        self.public_keys = []
+
+        # id of the user to which the task belongs
+        self.m_crypto_user_object_id = None
+
+        # data to operate on
+        self.m_process_data_p64s = None
+
+        # delete the task when completed
+        self.m_delete_me_when_done = True
+
         self.object_type = "CryptoTask"
-        super(CryptoTask, self).__init__(dbase=dbase,
-                                         comment="this object represents a command and stores intermediary results",
-                                         object_id=object_id)
 
         self.m_command_object = self.get_object_type()
         self.m_created_time = time.time()
         self.m_crypto_user_object_id = crypto_user_object_id
         self.m_delete_me_when_done = True
 
+        super(CryptoTask, self).__init__(dbase=dbase,
+                                comment="this object represents a command and stores intermediary results",
+                                object_id=object_id)
+
     def display(self):
         """ display string """
 
         return self.m_command_object + " / " + self.object_id
-
-    #def save(self, *argc, **argv):
-        """
-            save the task
-        @param argc:
-        @param *argc:
-        @param argv:
-        @param **argv: 
-        """
-
-    #    super(CryptoTask, self).save(*argc, **argv)
-    #    return self.object_id
 
     def total_execution_time(self):
         """ calculate total time """
