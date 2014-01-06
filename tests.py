96b00304e01b8cc71c1ba04b24688a35fe964af9
Erik de Jonge <erik@a8.nl>
2014-01-06 16:04:24 +0100

    Mon Jan  6 16:04:24 CET 2014

--- tests.py
+++ tests.py
@@ -6,20 +6,27 @@ __author__ = 'rabshakeh'
 import unittest
 from __init__ import *
 from couchdb_api import gds_delete_item_on_key, gds_get_scalar_list, gds_get_key_name
+
+
 class AddNumers(CryptoTask):
     """
     AddNumers
     """
+
     def run(self):
         """
         run
         """
         return self.m_process_data_p64s["v1"] + self.m_process_data_p64s["v2"]
+
 #noinspection PyPep8Naming
+
+
 class CryptoTaskTest(unittest.TestCase):
     """
     CryptoboTestCase
     """
+
     def setUp(self):
         """
         setUp
@@ -28,13 +35,17 @@ class CryptoTaskTest(unittest.TestCase):
         import couchdb_api
         self.all_servers = ['http://127.0.0.1:5984/']
         self.db_name = 'crypto_task_test'
+
         for server in self.all_servers:
             if self.db_name in list(couchdb.Server(server)):
                 couchdb.Server(server).delete(self.db_name)
+
         for server in self.all_servers:
             if self.db_name not in list(couchdb.Server(server)):
                 couchdb.Server(server).create(self.db_name)
+
         self.dbase = couchdb_api.CouchDBServer(self.db_name, self.all_servers, memcached_server_list=["127.0.0.1:11211"])
+
     def tearDown(self):
         """
         tearDown
@@ -42,23 +53,29 @@ class CryptoTaskTest(unittest.TestCase):
         for keyid in gds_get_scalar_list(self.db_name, member="keyval"):
             print "tests.py:54", gds_get_key_name(keyid)
             gds_delete_item_on_key(self.db_name, keyid)
+
     def test_many_task(self):
         """
         test_many_task
         """
         tasks = []
+
         for i in range(0, 100):
             task = AddNumers(self.dbase, "user_1234")
             task.m_delete_me_when_done = False
             task.m_process_data_p64s = {"v1": 5,
                                         "v2": 5}
+
             task.start()
             tasks.append(task)
+
         self.cronjob = subprocess.Popen(["/usr/bin/python", "cronjob.py"], cwd="/Users/rabshakeh/workspace/cryptobox/crypto_taskworker")
+
         for t in tasks:
             t.join()
             self.assertEqual(t.m_result, 10)
         self.cronjob.terminate()
+
     def test_task(self):
         """
         test_task
@@ -66,8 +83,10 @@ class CryptoTaskTest(unittest.TestCase):
         task = AddNumers(self.dbase, "user_1234")
         with self.assertRaisesRegexp(TypeError, "NoneType' object has no attribute '__getitem__'"):
             task.run()
+
         task.m_process_data_p64s = {"v1": 5,
                                     "v2": 5}
+
         result = task.run()
         self.assertEqual(result, 10)
         task.start()
@@ -87,6 +106,8 @@ class CryptoTaskTest(unittest.TestCase):
         task5.load(object_id=task.object_id)
         with self.assertRaisesRegexp(Exception, "There is no callable saved in this object"):
             self.assertIsNone(task5.execute())
+
+
 if __name__ == '__main__':
     print "tests.py:112", 'crypto_task unittest'
     unittest.main()
